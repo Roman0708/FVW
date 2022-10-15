1d982036ff4100480a3dad4cfcc21b7e86e5e181
Roman Kalimov <Roman.Kalimov@icl-services.com>
2022-10-15 05:00:39 +0300

    Initial commit

--- Final_task/Program.cs
+++ Final_task/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var initialArray = new string [] {"hello","2","world",":-)"};
+
+int length = initialArray.GetLength(0);
+//int count = 0;
+
+var resultArray = new string[] {};
+
+for (int count = 0; count < length; count++)
+{
+    if (initialArray[count].Length <= 3)
+    {
+        resultArray[resultArray.GetLength(0)] = initialArray[count];
+        Array.Resize(ref resultArray, resultArray.Length + 1);
+    }
+}
+
