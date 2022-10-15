5c66ad4cb680ee332bb38aa550e2dff643798504
Roman Kalimov <Roman.Kalimov@icl-services.com>
2022-10-15 05:37:43 +0300

    Working variant

--- Final_task/Program.cs
+++ Final_task/Program.cs
@@ -1,10 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-var initialArray = new string [] {"hello","2","world",":-)"};
+﻿var initialArray = new string [] {"Russia","Denmark","Kazan","-2"};
 
 int length = initialArray.GetLength(0);
-//int count = 0;
 
 var resultArray = new string[] {};
 
@@ -12,8 +8,10 @@ for (int count = 0; count < length; count++)
 {
     if (initialArray[count].Length <= 3)
     {
-        resultArray[resultArray.GetLength(0)] = initialArray[count];
         Array.Resize(ref resultArray, resultArray.Length + 1);
+        resultArray[resultArray.GetLength(0) - 1] = initialArray[count];
     }
 }
 
+if (resultArray.Length == 0) Console.WriteLine("Нечего выводить");
+else for (int a = 0; a < resultArray.GetLength(0); a++) Console.Write($"{resultArray[a]}, ");
